5bac0b540353798e7edc97f0671b7c3b03fcd184
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2024-07-17 23:29:23 +0100

    implemented a play round function that compares the choices of the player and computer to decide a winner

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice()
 {
     switch (Math.floor(Math.random() * 3)) {
@@ -26,3 +29,50 @@ function getHumanChoice()
             return 'incorrect'
     }
 }
+
+function playRound(humanChoice, computerChoice)
+{
+    if (humanChoice == 'incorrect' || computerChoice == 'Not Working')
+    {
+        console.log('Something went wrong try again')
+    }
+    else if (humanChoice === computerChoice)
+    {
+        console.log('You drew with the computer')
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'paper')
+    {
+        console.log('You lose this round, computer chose paper')
+        computerScore++
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'scissors')
+    {
+        console.log('You win this round! Computer chose scissors')
+        humanScore++
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'rock')
+    {
+        console.log('You lose this round, computer chose rock')
+        computerScore++
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper')
+    {
+        console.log('You win this round! Computer chose paper')
+        humanScore++
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'scissors')
+    {
+        console.log('You lose this round, computer chose scissors')
+        computerScore++
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock')
+    {
+        console.log('You win this round! Computer chose rock')
+        humanScore++
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
