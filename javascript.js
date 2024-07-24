bd460006c9037064b0f470c5e49d018a6e155a2b
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2024-07-24 20:37:05 +0100

    added buttons for each selection and text to follow the outcome of each round. And added text for first to reach 5 wins

--- javascript.js
+++ javascript.js
@@ -23,50 +23,122 @@ function getHumanChoice()
             return 'scissors'
         case 'paper':
             return 'paper'
-        default:
-            return 'incorrect'
-    }
+            default:
+                return 'incorrect'
+            }
 }
+        
+let humanWins = 0
+let computerWins = 0
+        
+const rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+        
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'Scissors';
+        
+const paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+
+const runningScores = document.createElement('div');
+        
+const resultsDiv = document.createElement('div');
+
+document.body.appendChild(resultsDiv);
+document.body.appendChild(runningScores);
+document.body.appendChild(rockButton);
+document.body.appendChild(scissorsButton);
+document.body.appendChild(paperButton);
+        
+rockButton.addEventListener('click', () => playRound('rock', getComputerChoice()));
+scissorsButton.addEventListener('click', () => playRound('scissors', getComputerChoice()));
+paperButton.addEventListener('click', () => playRound('paper', getComputerChoice()));
+
+function winner() {
+    if (humanWins === 5 || computerWins === 5)
+        {
+            resultsDiv.appendChild(runningScores);
+
+            if (humanWins === 5)
+                {
+                    const finalWinner = document.createElement('p');
+                    finalWinner.textContent = 'Final winner is the human';
+                    resultsDiv.appendChild(finalWinner);
+                }
+            else
+                {
+                    const finalWinner = document.createElement('p');
+                    finalWinner.textContent = 'Final winner is the computer';
+                    resultsDiv.appendChild(finalWinner);
+                }
+            }
+        }
 
 function playRound(humanChoice, computerChoice)
 {
     if (humanChoice == 'incorrect' || computerChoice == 'Not Working')
     {
-        console.log('Something went wrong try again')
+        resultsDiv.textContent = 'Something went wrong try again'
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
     }
     else if (humanChoice === computerChoice)
     {
-        console.log('You drew with the computer')
+        resultsDiv.textContent = 'You drew with the computer'
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
     }
     else if (humanChoice === 'rock' && computerChoice === 'paper')
     {
-        console.log('You lose this round, computer chose paper')
-        return -1
+        resultsDiv.textContent = 'You lose this round, computer chose paper'
+        computerWins += 1;
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
+
     }
     else if (humanChoice === 'rock' && computerChoice === 'scissors')
     {
-        console.log('You win this round! Computer chose scissors')
-        return 1
+        resultsDiv.textContent = 'You win this round! Computer chose scissors'
+        humanWins += 1;
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
     }
     else if (humanChoice === 'scissors' && computerChoice === 'rock')
     {
-        console.log('You lose this round, computer chose rock')
-        return -1
+        resultsDiv.textContent = 'You lose this round, computer chose rock'
+        computerWins += 1;
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
+
     }
     else if (humanChoice === 'scissors' && computerChoice === 'paper')
     {
-        console.log('You win this round! Computer chose paper')
-        return 1
+        resultsDiv.textContent = 'You win this round! Computer chose paper'
+        humanWins += 1;
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
+
     }
     else if (humanChoice === 'paper' && computerChoice === 'scissors')
     {
-        console.log('You lose this round, computer chose scissors')
-        return -1
+        resultsDiv.textContent = 'You lose this round, computer chose scissors'
+        computerWins += 1;
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
+
     }
     else if (humanChoice === 'paper' && computerChoice === 'rock')
     {
-        console.log('You win this round! Computer chose rock')
-        return 1
+        resultsDiv.textContent = 'You win this round! Computer chose rock'
+        humanWins += 1;
+        runningScores.textContent = 'Human Score: ' + humanWins + ' Computer Score: ' + computerWins;
+        winner();
+
     }
 }
-
